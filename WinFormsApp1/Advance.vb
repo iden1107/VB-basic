e9d4b2973afdcb80fe0d76526da728463c382fb5
神農　甫 <iden1107@icloud.com>
2022-07-24 20:21:27 +0900

    comiit

---
+++ WinFormsApp1/Advance.vb
@@ -0,0 +1,354 @@
+﻿'---インターフェイス
+'中身の処理は記述されない
+'クラスで実装(記述)することで動作する
+'継承先でメソッドのオーバーライドを強制
+Interface ISample
+    Sub ShowMessage()
+End Interface
+
+Public Class Sample
+    Implements ISample 'インターフェースを実装
+    Public Sub ShowMessage() Implements ISample.ShowMessage
+        Debug.WriteLine("派生クラス")
+    End Sub
+End Class
+
+
+
+'---ジェネリック
+'引数の型を任意のものが入る
+'型を指定した例では、型が異なる場合エラーになる
+'クラスをインスタンス化した際、型だけ異なる場合に
+'その度クラスを作らなくてもよくなることがメリット
+Class Aaa
+    Public bbb As Integer
+End Class
+
+Public Sub Fun1()
+    Dim xxx = New Aaa()
+    xxx.bbb = 100
+    Debug.WriteLine(xxx.bbb) '100で作ったインスタンスではOKでも
+End Sub
+
+Public Sub Fun2()
+    Dim xxx = New Aaa()
+    xxx.bbb = "hoge"
+    Debug.WriteLine(xxx.bbb) '文字のインスタンスはエラーになる
+End Sub
+
+
+'ジェネリックを使用した場合
+Class Aaa(Of T)
+    Public bbb As T
+End Class
+
+
+Public Sub Afun()
+    Dim xxx As New Aaa(Of String)
+    xxx.bbb = "hoge"
+    Debug.WriteLine(xxx.bbb)
+End Sub
+
+Public Sub Bfun()
+    Dim xxx As New Aaa(Of Integer)
+    xxx.bbb = 100
+    Debug.WriteLine(xxx.bbb)
+End Sub
+
+
+
+'---名前空間
+'クラス名の衝突や、クラスの分類などに使える
+Namespace Aaa
+    Public Class Bbb
+        Public Sub Ccc()
+            Debug.WriteLine("Aaa名前空間のCccメソッド実行")
+        End Sub
+    End Class
+
+    Public Class Ddd
+        Public Sub Eee()
+            Debug.WriteLine("Aaa名前空間のDddメソッド実行")
+        End Sub
+    End Class
+End Namespace
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim xxx = New Aaa.Bbb()
+        xxx.Ccc()
+    End Sub
+End Class
+
+'---importによる名前空間
+Imports WinFormsApp1.Aaa
+
+Namespace Aaa
+    Public Class Bbb
+        Public Sub Ccc()
+            Debug.WriteLine("Aaa名前空間のCccメソッド実行")
+        End Sub
+    End Class
+
+    Public Class Ddd
+        Public Sub Eee()
+            Debug.WriteLine("Aaa名前空間のDddメソッド実行")
+        End Sub
+    End Class
+End Namespace
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'ここで名前空間を省略できる
+        Dim xxx = New Bbb()
+        xxx.Ccc()
+    End Sub
+End Class
+
+
+
+'---構造体
+'クラス（参照型）と似ている
+'値型。継承などができない
+'軽量のオブジェクトを表すのに適している
+'小規模のデータであれば、処理が早い
+'継承、イベントなどができない
+
+Private Structure Users
+    Public UserID As String
+    Public Name As String
+    Public Age As Integer
+    Public Sub setUsers(ByVal setVal1 As String, ByVal setVal2 As String, ByVal setVal3 As Integer)
+        UserID = setVal1
+        Name = setVal2
+        Age = setVal3
+    End Sub
+End Structure
+
+Shared Sub Main(args As String())
+    '構造体を宣言し、初期化
+    Dim sUsers As Users
+    sUsers.setUsers("A0001", "テストユーザ1", 24)
+End Sub
+
+
+
+'---列挙型
+'複数の定数を1つにまとめる
+'列名に対応した数値を割り当てることで数値として扱える
+
+Public Enum Color
+    RED
+    YELLOW
+    BLUE
+End Enum
+
+Module Module1
+    Sub Main()
+        Console.WriteLine(Color.RED) '0が出力される
+        Console.WriteLine(Color.YELLOW) '1が出力される
+        Console.WriteLine(Color.BLUE) '2が出力される
+    End Sub
+End Module
+
+'最初の数値を１から始めたい場合
+Enum Color
+    RED = 1
+    YELLOW
+    BLUE
+End Enum
+
+Module Module1
+    Sub Main()
+        Console.WriteLine(Color.RED) '1が出力される
+        Console.WriteLine(Color.YELLOW) '2が出力される
+        Console.WriteLine(Color.BLUE) '3が出力される
+    End Sub
+End Module
+
+'数字の割り当てを任意の値にする場合
+Enum Color
+    RED = 15
+    YELLOW = -2
+    BLUE = 542
+End Enum
+
+'数値ではなく名称で出力する場合
+Enum Color
+    RED
+    YELLOW
+    BLUE
+End Enum
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine(Color.RED.ToString)
+    End Sub
+End Module
+
+'名称をループで取り出す
+Module Program
+    Sub Main(args As String())
+        For Each s In [Enum].GetNames(GetType(Color))
+            Console.WriteLine(s)
+        Next
+    End Sub
+End Module
+
+'数値をループで取り出す
+Module Program
+    Sub Main(args As String())
+        For Each i In [Enum].GetValues(GetType(Color))
+            Console.WriteLine(i) '0 1 2
+        Next
+    End Sub
+End Module
+
+
+
+'---デリゲート
+'メソッド(関数)を変数のように扱う機能
+'デリゲートは”型”をつくることがポイント
+Public Class Form1
+    'デリゲートの宣言
+    Delegate Sub sample()
+
+    '関数を用意
+    Sub Aaa()
+        Debug.WriteLine("Aaaメソッド")
+    End Sub
+
+    Sub Bbb()
+        Debug.WriteLine("Bbbメソッド")
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '変数を用意し、関数を割り当てる
+        Dim Ccc As sample = AddressOf Aaa　'この時点では実行しないため　Bbb()としない
+        '変数で実行する
+        Ccc()
+    End Sub
+End Class
+
+'引数ありの場合
+'注　デリゲートと型と代入する関数の形式を同一にすること！
+Public Class Form1
+    'デリゲートの宣言
+    Delegate Function sample(arg As Integer) As Integer
+
+    '関数を用意
+    Public Function Aaa(arg As Integer)
+        Return arg * 10 '引数を10倍にするメソッド
+    End Function
+
+    Function Bbb(arg As Integer)
+        Return arg + 10  '10増やすメソッド
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '変数を用意し、関数を割り当てる
+        Dim Ccc As sample = AddressOf Aaa
+        Debug.WriteLine(Ccc(2))
+    End Sub
+End Class
+
+'マルチキャストデリゲート
+'一つのデリゲート変数に複数のデリゲートを登録
+Public Class Form1
+    'デリゲートの宣言
+    Delegate Sub sample()
+
+    '関数を用意
+    Sub Aaa()
+        Debug.WriteLine("Aaaメソッド")
+    End Sub
+
+    Sub Bbb()
+        Debug.WriteLine("Bbbメソッド")
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '変数を用意し、関数を割り当てる
+        Dim Ccc As sample = AddressOf Aaa
+        Dim Ddd As sample = AddressOf Bbb
+        'combineを使いCccとDddをCcc()にまとめることで複数のデリゲートを登録
+        Ccc = System.Delegate.Combine(Ccc, Ddd)
+        Ccc()
+    End Sub
+End Class
+
+
+
+'---ラムダ式
+'VBには匿名メソッドを作成する機能なし
+'省略した形式で記述できる
+
+'通常の形式
+Public Class Form1
+    'デリゲートの宣言
+    Delegate Sub Aaa()
+
+    '関数の用意
+    Sub Bbb()
+        Debug.WriteLine("hoge")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '用意した関数をCccに代入
+        Dim Ccc As Aaa = AddressOf Bbb
+        '実行
+        Ccc()
+    End Sub
+End Class
+
+'ラムダ式
+Public Class Form1
+    'デリゲートの宣言
+    Delegate Sub Aaa()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '代入する関数は用意せず、右辺に直接記述
+        'なのでaddressofが不要
+        Dim Ccc As Aaa = Sub() Debug.WriteLine("hoge")
+        '実行
+        Ccc()
+    End Sub
+End Class
+
+'引数ありver
+Public Class Form1
+    'デリゲートの宣言
+    Delegate Sub Aaa(arg As String)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Ccc As Aaa = Sub(x) Debug.WriteLine(x)
+        '実行
+        Ccc("hoge")
+    End Sub
+End Class
+
+
+
+'---LINQ
+'データの集合から取得したい情報を引っ張ってくるための仕組み
+Public Class Form1
+    Dim aaa() As Integer = {60, 30, 80}
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'リストの中から50以上のものを抽出
+        Dim result = From a In Aaa
+                     Where a >= 50
+
+        'ループで出力
+        For Each r In result
+            Debug.WriteLine(r)
+        Next
+    End Sub
+
+End Class
---
+++ WinFormsApp1/Class.vb
@@ -0,0 +1,315 @@
+﻿'クラス
+
+'変数（フィールド）
+'メソッド
+'上二つを合わせてメンバという
+Class Sample
+    Public aaa As String = "変数" 'Dimは省略可
+    Public Sub bbb()
+        Console.WriteLine("メソッド")
+    End Sub
+End Class
+
+'インスタンス化　sampleクラスからxxxを作り出す
+Module Program
+    Sub Main()
+        Dim xxx = New Sample()
+        xxx.bbb()　'メソッドの呼び出し
+    End Sub
+End Module
+
+
+
+'---コンストラクタ
+'インスタンス作成時に呼ばれるメソッド。初期化するときに使われる
+Class Sample
+    Public aaa As String
+    'コンストラクタでaaaの値をセット
+    Sub New()
+        'Me.aaa = "変数" 　　この形式でもよい
+        aaa = "変数"
+    End Sub
+
+    Public Sub bbb()
+        Console.WriteLine("メソッド")
+    End Sub
+End Class
+'インスタンス化
+Module Program
+    Sub Main()
+        Dim xxx = New Sample()
+        Console.WriteLine(xxx.aaa) 'aaaの"変数"が出力
+    End Sub
+End Module
+
+
+'インスタンスの時に値をセットする
+Class Sample
+    Public aaa As String
+    Sub New(ccc As String)　'②cccには"hoge"が格納
+        aaa = ccc　'③フィールドのaaaにcccが代入
+    End Sub
+
+    Public Sub bbb()
+        Console.WriteLine("メソッド")
+    End Sub
+End Class
+'インスタンス化
+Module Program
+    Sub Main()
+        Dim xxx = New Sample("hoge")　'①値をセット
+        Console.WriteLine(xxx.aaa) '①から③を経て"hoge"が出力
+    End Sub
+End Module
+
+
+'---オーバーロード
+'パラメーターの型や数が異なれば同じメソッド名で複数定義できる
+Class Sample
+    Public aaa As String
+    '①aaaを初期化する２つのコンストラクタがある
+    '②それぞれ型が異なって定義されている
+    Sub New(ccc As String)
+        aaa = "文字列です"
+    End Sub
+
+    Sub New(ccc As Integer)
+        aaa = "数値です"
+    End Sub
+End Class
+Module Program
+    Sub Main())
+        '③インスタンス生成時は"hoge"と文字列なのでこの場合は上のコードが適用される
+        'コンパイラが適用するものを分けている
+        Dim xxx = New Sample("hoge")
+        Console.WriteLine(xxx.aaa)
+    End Sub
+End Module
+
+
+'オーバーロード（メソッド）
+Class Sample
+    'bbb()と同名のメソッドが３つあるがそれぞれ引数が異なっている
+    Public Sub bbb(a As Integer)
+        Console.WriteLine(a)
+    End Sub
+
+    Public Sub bbb(a As Integer, b As Integer)
+        Console.WriteLine(a + b)
+    End Sub
+    Public Sub bbb(a As Integer, c As String)
+        Console.WriteLine(a & c)
+    End Sub
+End Class
+Module Program
+    Sub Main()
+        Dim xxx = New Sample()
+        xxx.bbb(10, 20)　'数値が２つ渡されているので真ん中が適用
+        xxx.bbb(1923, "年") '数値と文字が渡されているので下が適用 
+        '同じメソッド名を使いまわせるメリットがある
+    End Sub
+End Module
+
+
+
+'---継承
+Public Class Sample
+    Public Sub bbb()
+        Console.WriteLine("メソッド")
+    End Sub
+End Class
+
+
+Public Class SampleChild 'Sampleクラスから継承
+    Inherits Sample
+End Class
+
+
+
+'---オーバーライド
+Public Class Sample
+    Public Overridable Sub bbb()
+        Console.WriteLine("メソッド")
+    End Sub
+End Class
+
+Public Class SampleChild
+    Inherits Sample
+    Public Overrides Sub bbb()
+        Console.WriteLine("上書き")
+    End Sub
+End Class
+
+
+'---アクセス修飾子
+'Public クラス外部からも可能
+'Firend　同一プログラム、ソリューション内から可能
+'Protected　クラス内部、サブクラスから可能
+'Private　クラス内部のみ可能
+
+
+'---プロパティ
+'クラス内の変数（フィールド）はprivateで宣言することが推奨される
+'そのため変数にアクセスするにはメソッドを経由する方法がある
+'それを便利にするのがプロパティ
+Class Sample
+    Private aaa As String = "文字"
+End Class
+Module Program
+    Sub Main()
+        Dim xxx = New Sample()
+        Console.WriteLine(xxx.aaa)　'このままでは当然アクセスできな
+    End Sub
+End Module
+
+'そこでアクセサ(get,set)を使い値を操作できるようにする
+Class Sample
+    Private _number As Integer = 50
+    Public Property Number() As Integer
+        Get
+            Return _number
+
+        End Get
+        Set(value As Integer)
+            _number = value
+        End Set
+    End Property
+End Class
+Module Program
+    Sub Main()
+        Dim xxx = New Sample()
+        Console.WriteLine(xxx.Number) 'getの50が出力
+
+        xxx.Number = 100
+        Console.WriteLine(xxx.Number) '100がsetされたので100が出力
+    End Sub
+End Module
+
+'上記のようにgetとsetをそのまま返すだけではpubicでフィールドを設定することとさほど変わらない
+'setにロジックを加えることで値の設定に条件をつけることなどができる
+Class Sample
+    Private _number As Integer = 50
+    Public Property Number() As Integer
+        Get
+            Return _number
+
+        End Get
+        Set(value As Integer) '0でない場合のみ任意の値を設定できる
+            If value = Not 0 Then
+                _number = value
+            End If
+        End Set
+    End Property
+End Class
+
+'Getしかない時はReadOnlyを付ける
+Class Sample
+    Private _number As Integer = 0
+    Public ReadOnly Property Number() As Integer
+        Get
+            Return _number
+        End Get
+    End Property
+End Class
+
+'Setしかない時はWriteOnlyを付ける
+Class Sample
+    Private _number As Integer = 0
+    Public WriteOnly Property Number() As Integer
+        Set(ByVal value As Integer)
+            _number = value
+        End Set
+    End Property
+End Class
+
+'自動実装プロパティ　getとsetを省略できる記述方法
+'ロジックが必要な場合はgetとsetを記述しなければならない
+'自動実装プロパティと ReadOnly、WriteOnly キーワードの併用もできない
+Class Sample
+    Public Property Number() As Integer
+    'Public Property Number() As Integer = 100 初期値の設定もできる
+
+End Class
+Module Program
+    Sub Main()
+        Dim xxx = New Sample()
+        Console.WriteLine(xxx.Number)
+        xxx.Number = 200
+        Console.WriteLine(xxx.Number)
+    End Sub
+End Module
+
+
+
+'---既定のプロパティ (C#ではインデクサ)
+'クラスのインスタンスを配列のようにインデックスで呼び出せるようにしたもの
+Class Sample
+    Private arr() As String
+    Sub New(ByVal size As Integer)
+        arr = New String(size) {}
+    End Sub
+    Default Property Item(ByVal index As Integer) As String
+        Get
+            Return arr(index)
+        End Get
+        Set(ByVal value As String)
+            arr(index) = value
+        End Set
+    End Property
+End Class
+
+Module Program
+    Sub Main()
+        Dim xxx = New Sample(3)
+        xxx(1) = "hoge"
+        xxx(2) = "foo"
+        xxx(3) = "hogehoge"
+
+        Console.WriteLine(xxx(2))
+    End Sub
+End Module
+
+
+'--- moduleクラス(C#:staticクラス)
+' shared修飾子(C#:static修飾子)
+'モジュールは、メンバが暗黙的に Shared である型
+'オブジェクト指向ではないため使用は推奨されない
+'Shared修飾子を付与した変数は、インスタンスを作成しなくても存在
+
+
+Class Sample
+    Public Shared aaa As String = "変数"
+    Public Shared Sub bbb()
+        Debug.WriteLine("メソッド")
+    End Sub
+End Class
+
+
+Sub Main()
+    Console.WriteLine(Sample.aaa)
+    'インスタンスを生成せずに直接使える
+    bbb() 'メソッドも同様に直接呼びだし
+End Sub
+
+
+'---抽象クラス
+'複数人で開発を行う場合に実装レベルのルールを作れる意図
+'継承してもらうことを前提にしているクラスで、それ自身をインスタンス化することはできない
+'継承された派生クラスでは抽象メソッドを必ずオーバーライドしなければならない制約がある
+Public MustInherit Class Sample '①抽象クラス
+    Public MustOverride Sub Method1()
+End Class
+
+Public Class Class　'②継承した具象クラス
+    Inherits Sample
+    Public Overrides Sub Method1()
+        Debug.WriteLine("hoge")
+    End Sub
+End Class
+
+
+Private Sub main()
+    '③具象クラスからインスタンス化
+    Dim xxx = New ClassSample()
+    xxx.Method1()
+End Sub
